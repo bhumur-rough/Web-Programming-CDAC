656ea298921dd08608502185b2aba386fcf29076
bhumur-rough <agrawalbhumur@gmail.com>
2025-05-30 19:35:35 +0530

    random

--- Practice/server.js
+++
@@ -1,7 +0,0 @@
-const http = require("http")
-
-var server = http.createServer(function(req,res){
-    res.write("Hi this out first server")
-    res.end()
-})
-server.listen(3000,()=>{console.log("Server is running at 3000")})
